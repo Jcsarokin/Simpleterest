8909b05486397704b59ca46bdd580453ec181821
Julian Sarokin <Julian@sellsimple.com>
2014-02-01 20:44:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simpletrest::Application.config.secret_key_base = '2242f1243b7385cf44d7c9977fdf45a8b4c950c0cd8219221e0152bb19aec56f03370c3680c395e6c901c905d735ae2111c324552c4b3feb692f561089419f8f'
